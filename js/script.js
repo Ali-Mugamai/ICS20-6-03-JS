bd52496c680bdf8296efb36ec13d383430b526f1
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-12-20 14:10:59 -0500

    RGDGDR

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
-// Copyright (c) 2020 ali-mugamai All rights reserved
+// Copyright (c) 2022 Ali Mugamai All rights reserved
 //
-// Created by: ali mugamai
+// Created by: Ali Mugamai
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
-
-"use strict"
 
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-6-03-JS/sw.js", {
+    scope: "/ICS20-6-03-JS/",
   })
 }
 
+
 /**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
