db1e8f3fe676feb822e1d32bede92270f9e50965
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-12-21 19:15:00 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -19,12 +19,13 @@ const getImage = async (URLAddress) => {
     const jsonData = await result.json()
     console.log(jsonData)
     let celcius = 0
-    document.getElementById("api-weather").innerHTML =
-    celcius = jsonData.weather - 273.15
-  
+    document.getElementById("api-weather").innerHTML = celcius =
+      jsonData.weather - 273.15
   } catch (err) {
     console.log(err)
   }
 }
 
-getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+getImage(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
